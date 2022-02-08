25d61ea69ab5dfda27da2863dbf2b418af1a431d
R00T80Y <R00T80Y@gmail.com>
2022-02-09 01:00:18 +0300

    Add Hooks

--- pluginName.plugin.js
+++ pluginName.plugin.js
@@ -1,32 +1,37 @@
 /**
  * @author r00t80y<https://github.com/R00T80Y>
- * @file
+ * @file JavaScript Plugin Template
  * @since 04-02-2022
- * @updated 04-02-2022
- * @version 0.0.1
+ * @updated 09-02-2022
+ * @version 0.1.0
  */
 
+import './utils.js';
+
 const defaultOptions = {
-  // Plugin options here
+  // Plugin options here...
+
+  // Plugin Hooks
+  init: false,
+  destroy: false,
 };
 
-function Plugin($element, pluginOptions) {
+function Plugin($rootElement, pluginOptions) {
 
   // Private variables
-  let $root = $element;
+  let $root = $rootElement;
 
-  init();
+  // Plugin code here...
 
-  // Plugin code here
-
-  function init() {
-    // Plugin init code here
-  }
+  // Hook init
+  Utils.type(pluginOptions.init) === 'function' && pluginOptions.init();
 
   // Public method for destroying a plugin
   function destroy() {
     // Remove events
     // Remove created tags
+    // Hook destroy
+    Utils.type(pluginOptions.destroy) === 'function' && pluginOptions.destroy();
     return true;
   }
 
