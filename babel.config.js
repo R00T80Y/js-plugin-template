bb3d4234ad3b3389d072a3ec77b83df81e272fce
R00T80Y <R00T80Y@gmail.com>
2022-02-24 22:50:16 +0300

    Create project structure: Config files for babel and eslint

---
+++ babel.config.js
@@ -0,0 +1,11 @@
+const PRODUCTION_MODE = !!(process.env.NODE_ENV === 'production');
+
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env', {
+        debug: PRODUCTION_MODE
+      }
+    ]
+  ]
+};
